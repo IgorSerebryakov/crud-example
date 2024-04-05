a1a821f617a5d1c269f1f8aa446ca92971fc2b18
IgorSerebryakov <serebryakov09@mail.ru>
2024-04-05 17:49:40 +0300

    AUTHENTIFICATION

--- public/index.php
+++ public/index.php
@@ -1,45 +1,68 @@
 <?php
 
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-use DI\Container;
-use Slim\Factory\AppFactory;
+session_start();
 
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
 
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
 
-$app->get('/schools', function ($request, $response) {
-    $data = file_get_contents("schools.json");
-    $schools = json_decode($data, true);
-    $params = [
-        'schools' => $schools
-    ];
+$users = [
+    ['name' => 'admin', 'passwordDigest' => hash('sha256', 'secret')],
+    ['name' => 'mike', 'passwordDigest' => hash('sha256', 'superpass')],
+    ['name' => 'kate', 'passwordDigest' => hash('sha256', 'strongpass')]
+];
 
-    return $this->get('renderer')->render($response, "schools/index.phtml", $params);
-})->setName('schools');
+// BEGIN (write your solution here)
+$app->get('/', function ($request, $response) {
+    $messages = $this->get('flash')->getMessages();
+    if (isset($_SESSION['user'])) {
+        $authenticatedUser = $_SESSION['user'];
+        $params = [
+            'authenticatedUser' => $authenticatedUser,
+            'flash' => $messages
+        ];
 
-$app->get('/schools/{id}', function ($request, $response, $args) {
-    $id = $args['id'];
-    $data = file_get_contents("schools.json");
-    $schools = json_decode($data, true);
-    foreach ($schools as $school) {
-        if ($school['id'] === $id) {
-            $params = [
-                'school' => $school
-            ];
-            
-            return $this->get('renderer')->render($response, 'schools/show.phtml', $params);
-        }
+        return $this->get('renderer')->render($response, 'index.phtml', $params);
     }
     
-    return $response->write('Page not found')->withStatus(404);
-})->setName('school');
+    $params = [
+        'flash' => $messages
+    ];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
+});
 
+$app->post('/session', function ($request, $response) use ($users) {
+    $user = $request->getParsedBodyParam('user');
+    $encryptedPass = hash('sha256', $user['password']);
+
+    if (in_array(['name' => $user['name'], 'passwordDigest' => $encryptedPass], $users)) {
+        $_SESSION['user'] = $user;
+    } else {
+        $this->get('flash')->addMessage('warning', 'Wrong password or name');
+    }
+
+    return $response->withRedirect('/');
+});
+
+$app->delete('/session', function ($request, $response) {
+    $_SESSION = [];
+    session_destroy();
+    return $response->withRedirect('/');
+});
+// END
 $app->run();
-
